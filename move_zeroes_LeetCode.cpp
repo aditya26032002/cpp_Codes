99460ad402eb6fc16000dd611814297cf9c08d09
aditya26032002 <imadityasahni101@gmail.com>
2023-01-12 20:45:51 +0530

    .

--- move_zeroes_LeetCode.cpp
+++ move_zeroes_LeetCode.cpp
@@ -2,28 +2,14 @@
 using namespace std;
 
 void moveZeroes(vector<int>& nums) {
-    vector<int> notZeroes;
-    int countZeroes = 0;
     int i = 0;
-    while(i<nums.size()){
-        if(nums[i] != 0){
-            notZeroes.push_back(nums[i]);
+    for(int j = 0; j<nums.size(); j++){
+        if(nums[j] != 0){
+            swap(nums[i], nums[j]);
+            i++;
         }
-        else
-            countZeroes++;
-        i++;
     }
-    while(countZeroes){
-        notZeroes.push_back(0);
-        countZeroes--;
-    }
-    nums = notZeroes;
 }
-
-// void moveZeroes(vector<int>& nums) {
-//     int i = 0, j = i+1;
-//     while(j)
-// }
 
 int main(){
     vector<int> nums = {0,1,0,3,12};
@@ -33,3 +19,43 @@ int main(){
     }
     return 0;
 }
+
+// void moveZeroes(vector<int>& nums) {
+//     vector<int> notZeroes;
+//     int countZeroes = 0;
+//     int i = 0;
+//     while(i<nums.size()){
+//         if(nums[i] != 0){
+//             notZeroes.push_back(nums[i]);
+//         }
+//         else
+//             countZeroes++;
+//         i++;
+//     }
+//     while(countZeroes){
+//         notZeroes.push_back(0);
+//         countZeroes--;
+//     }
+//     nums = notZeroes;
+// }
+
+// void moveZeroes(vector<int>& nums) {
+//     int i = 0, j = 0;
+//     while(j<nums.size() && i<nums.size()){
+//         if(j == nums.size()-1)
+//             break;
+//         if(nums[i] != 0){
+//             i++;
+//         }
+//         else{
+//             j = i+1;
+//             while(j<nums.size()){
+//                 if(nums[j] != 0){
+//                     swap(nums[i],nums[j]);
+//                     break;
+//                 }
+//                 j++;
+//             }
+//         }
+//     }
+// }
