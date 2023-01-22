f53516d8028f66623731bb1d8278f992557cf8b8
aditya26032002 <imadityasahni101@gmail.com>
2023-01-23 00:21:17 +0530

    .

--- ArrayCodingNinjasIntersectionOf2Arrays.cpp
+++ ArrayCodingNinjasIntersectionOf2Arrays.cpp
@@ -1,14 +1,12 @@
-#include<iostream>
-// #include <bits/stdc++.h> //error
+#include <bits/stdc++.h>
 using namespace std;
 
-#define INT_MIN (-2147483647 - 1)
-
-int main(){
-    int arr1[] = {1,2,3};
-    int len1 = sizeof(arr1)/sizeof(arr1[0]);
-    int arr2[] = {3,4,5};
-    int len2 = sizeof(arr2)/sizeof(arr2[0]);
+int main()
+{
+    int arr1[] = {1, 2, 3};
+    int len1 = sizeof(arr1) / sizeof(arr1[0]);
+    int arr2[] = {3, 4, 5};
+    int len2 = sizeof(arr2) / sizeof(arr2[0]);
 
     // Non optimized
     // for (int i = 0; i < len1; i++)
@@ -23,25 +21,29 @@ int main(){
     //             arr2[j] = INT_MIN;
     //             break;
     //         }
-            
+
     //     }
     // }
 
     // Optimized
-    int i =0, j = 0;
-    while((i < len1) & (j < len2)){
-        if(arr1[i] < arr2[j]){
+    int i = 0, j = 0;
+    while ((i < len1) & (j < len2))
+    {
+        if (arr1[i] < arr2[j])
+        {
             i++;
         }
-        else if(arr1[i] == arr2[j]){
-            cout<<arr1[i]<<" ";
+        else if (arr1[i] == arr2[j])
+        {
+            cout << arr1[i] << " ";
             i++;
             j++;
         }
-        else{
+        else
+        {
             j++;
         }
     }
-    
+
     return 0;
 }
