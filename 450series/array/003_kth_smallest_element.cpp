04597e252fcddce6a59cc14e9a6cfbb0eb5680c5
aditya26032002 <imadityasahni101@gmail.com>
2023-01-22 21:32:09 +0530

    .

--- 450series/array/003_kth_smallest_element.cpp
+++ 450series/array/003_kth_smallest_element.cpp
@@ -1,7 +1,15 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    
+int kthSmallest(int arr[], int l, int r, int k)
+{
+    sort(arr, arr + r - 1);
+    return arr[k - 1];
+}
+
+int main()
+{
+    int arr[] = {7, 10, 4, 3, 20, 15};
+    cout << kthSmallest(arr, 0, 5, 3);
     return 0;
 }
